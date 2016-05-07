9f21029dbf8123c11f748655cbdd3764e74abec5
Brian Wilcox <briwilcox@users.noreply.github.com>
2016-05-06 23:37:38 -0500

    go format

--- binarysearch.go
+++ binarysearch.go
@@ -5,17 +5,17 @@ import (
 )
 
 func binSearch(searchspace []int, key int) int {
-	var min, max int 
-	min = searchspace[0] 
-	max = searchspace[len(searchspace)-1] 
+	var min, max int
+	min = searchspace[0]
+	max = searchspace[len(searchspace)-1]
 	for {
 		if max < min {
 			return -1
 		}
-		m:= (min + max)/2 
-		if (searchspace[m] < key) {
+		m := (min + max) / 2
+		if searchspace[m] < key {
 			min = m + 1
-		} else if (searchspace[m] > key) {
+		} else if searchspace[m] > key {
 			max = m - 1
 		} else {
 			return m
@@ -23,21 +23,21 @@ func binSearch(searchspace []int, key int) int {
 	}
 }
 
-func printSearchResult(a []int, key int){
-	fmt.Println("Binary Search:") 
-	index := binSearch(a, key) 
+func printSearchResult(a []int, key int) {
+	fmt.Println("Binary Search:")
+	index := binSearch(a, key)
 	if index == -1 {
 		fmt.Println("Search Space: ", a)
-		fmt.Println(key,"was not found")
+		fmt.Println(key, "was not found")
 	} else {
 		fmt.Println("Search Space: ", a)
-		fmt.Println("a[",index, "] = ", a[index])
+		fmt.Println("a[", index, "] = ", a[index])
 	}
 	fmt.Println("")
 }
 
 func main() {
-	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9} 
+	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	printSearchResult(a, 9)
 	printSearchResult(a, 2)
 	printSearchResult(a, 15)
